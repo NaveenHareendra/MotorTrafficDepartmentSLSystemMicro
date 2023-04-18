5199cc534ca63f8c0edc67e14a44d84e2d250a90
Yashmika Anusara <yashmikaaunusara@gmail.com>
2023-04-18 12:23:57 +0530

    Vehicle Registration

---
+++ Vehicle.Registration/Data/VehicleDataService.cs
@@ -0,0 +1,12 @@
+﻿namespace Vehicle.Registration.Data
+{
+    public class VehicleDataService
+    {
+        public static List<Models.Vehicle> Vehicles = new List<Models.Vehicle>()
+        {
+            new Models.Vehicle{Id=1,Vehiclebrand="BMW",Vehiclemodel="740le",Vehiclenumber="CBE-5698",Vehicleowner="Kasun",Vehicletype="Luxury Car"},
+            new Models.Vehicle{Id=2,Vehiclebrand="Mercedes-Benz",Vehiclemodel="S 650",Vehiclenumber="CAW-2335",Vehicleowner="Amala",Vehicletype="Luxury Car"},
+            new Models.Vehicle{Id=3,Vehiclebrand="Toyota",Vehiclemodel="Crown",Vehiclenumber="CBJ-2643",Vehicleowner="Nimal",Vehicletype="Luxury Car"},
+        };
+    }
+}
---
+++ Vehicle.Registration/Models/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace Vehicle.Registration.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string? Vehiclebrand { get; set; }
+        public string? Vehiclemodel { get; set; }
+        public string? Vehicletype { get; set; }
+        public string? Vehicleowner { get; set; }
+        public string? Vehiclenumber { get; set; }
+    }
+}
